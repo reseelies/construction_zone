8895ff04941763751d104fd5874aae85e2d045ae
Luis Rangel DaCosta <luisrd@berkeley.edu>
2024-07-02 13:58:17 -0700

    add in class for creating sets based on equality operations rather than hashing; add corresponding tests

---
+++ czone/util/eset.py
@@ -0,0 +1,170 @@
+from collections.abc import Iterable
+from collections import deque
+import numpy as np
+from typing import Union
+from functools import reduce
+
+class EqualSet():
+    """Creates a set based on equality operations.
+    For arrays, by default considers arrays unordered by axis 0.
+    
+    """
+    def __init__(self, x=[]):
+        self._storage = deque([])
+        self.update(x)
+
+    def __iter__(self):
+        yield from iter(self._storage)
+
+    def __len__(self):
+        return len(self._storage)
+    
+    def __contains__(self, item):
+        return item in self._storage
+    
+    @staticmethod
+    def _make_unique(x: Iterable):
+        match x:
+            case EqualSet():
+                yield from x
+            case _:
+                N_items = len(x)
+                add_dict = {}
+                for i in range(N_items):
+                    add_dict[i] = True if i not in add_dict else False
+                    if add_dict[i]:
+                        for j in range(i+1, N_items):
+                            if x[j] == x[i]:
+                                add_dict[j] = False
+                yield from (val for k, val in zip(sorted(add_dict.keys()), x) if add_dict[k])
+
+    def _check_if_new(self, other):
+        for o in other:
+            add = True
+            for s in self:
+                if o == s:
+                    add = False
+                    break
+            if add:
+                yield o
+    
+    def update(self, other: Iterable):
+        tmp = self._make_unique(other)
+        if len(self) == 0:
+            self._storage.extend(tmp)
+        else:
+            self._storage.extend(self._check_if_new(tmp))
+
+    def union(self, other: Iterable):
+        res = EqualSet(self)
+        res.update(other)
+        return res
+    
+    def __or__(self, other):
+        return self.union(other)
+    
+    def __ior__(self, other):
+        self.update(other)
+        return self
+    
+    def remove(self, other: Iterable):
+        tmp = self._make_unique(other)
+        for t in tmp:
+            try:
+                self._storage.remove(t)
+            except ValueError:
+                pass
+    
+    def difference(self, other: Iterable):
+        res = EqualSet(self)
+        res.remove(other)
+        return res
+    
+    def __sub__(self, other):
+        return self.difference(other)
+    
+    def __isub__(self, other):
+        self.remove(other)
+        return self
+    
+    def symmetric_difference(self, other: Iterable):
+        A = EqualSet(other)
+        return (self - A).union(A - self)
+    
+    def __xor__(self, other: Iterable):
+        return self.symmetric_difference(other)
+    
+    def intersection(self, other: Iterable):
+        A = self - other
+        return self - A
+    
+    def __and__(self, other: Iterable):
+        return self.intersection(other)
+
+    def isdisjoint(self, other):
+        return len(self.intersection(other)) == 0
+    
+    def issubset(self, other):
+        return reduce(lambda x, y: x and y, [i in other for i in self])
+    
+    def __le__(self, other):
+        return self.issubset(other)
+    
+    def issuperset(self, other):
+        return reduce(lambda x, y: x and y, [i in self for i in other])
+    
+    def __ge__(self, other):
+        return self.issuperset(other)
+
+    def __eq__(self, other):
+        return (self <= other) and (self >= other)
+
+    def __lt__(self, other):
+        return (self <= other) and (self != other)
+    
+    def __gt__(self, other):
+        return (self >= other) and (self != other)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def set_equal(A, B, **kwargs):
+#     """Set equality for groups of unhashable types.
+#     Checks __eq__ for objects.
+#     For numpy arrays, checks that each item A[i, ...] is equal to some item B[j, ...],
+
+#     i.e., that A, B are unordered along axis 0 but ordred along other axes.
+#     """
+#     match A:
+#         case np.ndarray():
+#             if not isinstance(B, np.ndarray):
+#                 return False
+            
+#             if A.shape == B.shape:
+#                 A_sorted = np.sort(A, axis=0)
+#                 B_sorted = np.sort(B, axis=0)
+#                 return np.allclose(A_sorted, B_sorted, **kwargs)
+#             else:
+#                 return False
+#         case Iterable():
+
+#             ## reduce A and B to proper sets by ==
+#             A_dict = [id(a) for a in A]
+#             B_dict = [id(b) for b in B]
+
+
+
+#         case _:
+#             return A == B
+
---
+++ tests/test_util.py
@@ -0,0 +1,128 @@
+import unittest
+import numpy as np
+from test_utils import czone_TestCase
+
+from czone.util.eset import EqualSet
+
+seed = 9817924
+rng = np.random.default_rng(seed=seed)
+
+class Test_EqualSet(czone_TestCase):
+
+    def setUp(self):
+        self.N_trials = 256
+        self.N_points = 256
+
+    def assertSetEquivalent(self, first, second, msg=None) -> None:
+        "Fail if two sets are not equal, checking "
+        self.assertEqual(len(first), len(second), msg)
+        self.assertEqual(first.union(second), first, msg)
+        self.assertEqual(first.symmetric_difference(second), set([]), msg)
+
+
+    def test_init(self):
+        for _ in range(self.N_trials):
+            x = rng.integers(0, 100, size=(self.N_points))
+
+            sx_ref = set([i for i in x])
+
+            sx_test = EqualSet(x)
+            self.assertSetEquivalent(sx_ref, sx_test)
+
+    def test_union(self):
+        for _ in range(self.N_trials):
+            x = rng.integers(0, 100, size=(self.N_points))
+            y = rng.integers(0, 100, size=(self.N_points))
+
+            sx_ref = set([i for i in x]).union([j for j in y])
+            sx_test = EqualSet(x).union([j for j in y])
+            sx_itest = EqualSet(x)
+            sx_itest |= [j for j in y]
+
+            self.assertSetEquivalent(sx_ref, sx_test)
+            self.assertSetEquivalent(sx_ref, sx_itest)
+            self.assertSetEquivalent(sx_ref, EqualSet(x) | [j for j in y])
+
+
+    def test_difference(self):
+        for _ in range(self.N_trials):
+            x = rng.integers(0, 100, size=(self.N_points))
+            y = rng.integers(0, 100, size=(self.N_points))
+
+            sx_ref = set([i for i in x]).difference([j for j in y])
+            sx_test = EqualSet(x).difference([j for j in y])
+            sx_itest = EqualSet(x)
+            sx_itest -= [j for j in y]
+
+            self.assertSetEquivalent(sx_ref, sx_test)
+            self.assertSetEquivalent(sx_ref, sx_itest)
+            self.assertSetEquivalent(sx_ref, EqualSet(x) - [j for j in y])
+
+    def test_symmetric_difference(self):
+        for _ in range(self.N_trials):
+            x = rng.integers(0, 100, size=(self.N_points))
+            y = rng.integers(0, 100, size=(self.N_points))
+
+            sx_ref = set([i for i in x]).symmetric_difference([j for j in y])
+            sx_test = EqualSet(x).symmetric_difference([j for j in y])
+            
+            self.assertSetEquivalent(sx_ref, sx_test)
+            self.assertSetEquivalent(sx_ref, EqualSet(x) ^ [j for j in y])
+
+    def test_intersection(self):
+        for _ in range(self.N_trials):
+            x = rng.integers(0, 100, size=(self.N_points))
+            y = rng.integers(0, 100, size=(self.N_points))
+
+            sx_ref = set([i for i in x]).intersection([j for j in y])
+            sx_test = EqualSet(x).intersection([j for j in y])
+
+            self.assertSetEquivalent(sx_ref, sx_test)
+            self.assertSetEquivalent(sx_ref, EqualSet(x) & [j for j in y])
+
+    def test_isdisjoint(self):
+        for _ in range(self.N_trials):
+            x = rng.integers(0, 100, size=(self.N_points))
+            y = rng.integers(0, 100, size=(self.N_points))
+
+            ref_A = set([i for i in x])
+            ref_B = set([j for j in y])
+            ref_A.difference_update(ref_B)
+        
+            true_check = EqualSet(list(ref_A)).isdisjoint(list(ref_B))
+            self.assertTrue(ref_A.isdisjoint(ref_B))
+            self.assertTrue(true_check)
+
+            ref_A.update(ref_B)
+            false_check = EqualSet(list(ref_A)).isdisjoint(list(ref_B))
+            self.assertFalse(ref_A.isdisjoint(ref_B))
+            self.assertFalse(false_check)
+
+    def test_equalities(self):
+        for _ in range(self.N_trials):
+            x = rng.integers(0, 100, size=(self.N_points))
+            y = rng.integers(0, 100, size=(self.N_points))
+
+            sx_ref = set([i for i in x])
+            sy_ref = set([j for j in y])
+            sx_test = EqualSet(x)
+            sy_test = EqualSet(y)
+
+            ## subset, superset
+            self.assertEqual(sx_ref.issubset(sy_ref), sx_test.issubset(sy_test))
+            self.assertEqual(sx_ref.issuperset(sy_ref), sx_test.issuperset(sy_test))
+            self.assertEqual(sx_ref <= sy_ref, sx_test <= sy_test)
+            self.assertEqual(sx_ref >= sy_ref, sx_test >= sy_test)
+
+            ## subset AND superset
+            self.assertEqual(sx_ref == sy_ref, sx_test == sy_test)
+            self.assertTrue(sx_test == EqualSet(x))
+
+            ## proper subset/supersets
+            self.assertTrue((sx_test | sy_test) > sx_test)
+            self.assertTrue((sx_test | sy_test) > sy_test)
+            self.assertTrue(sx_test < (sx_test | sy_test))
+            self.assertTrue(sy_test < (sx_test | sy_test))
+
+
+
